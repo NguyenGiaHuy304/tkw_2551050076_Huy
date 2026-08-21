e0ed69680291fce9ee3156168e0a13a24a65677f
NgwThanhH1 <nguyenhuy2007.pc@gmail.com>
2026-08-21 22:45:34 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,23 @@ import { resolve } from 'path';
 
 export default defineConfig({
   base: './',
+
   server: {
     port: 5175,
     open: true
   },
+
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, 'pages/about.html'),
-        skills: resolve(__dirname, 'pages/skills.html'),
-        projects: resolve(__dirname, 'pages/projects.html'),
-        projectDetail: resolve(__dirname, 'pages/project-detail.html'),
-        blog: resolve(__dirname, 'pages/blog.html'),
-        blogDetail: resolve(__dirname, 'pages/blog-detail.html'),
-        contact: resolve(__dirname, 'pages/contact.html'),
+        about: resolve(__dirname, 'about.html'),
+        skills: resolve(__dirname, 'skills.html'),
+        projects: resolve(__dirname, 'projects.html'),
+        projectDetail: resolve(__dirname, 'project-detail.html'),
+        blog: resolve(__dirname, 'blog.html'),
+        blogDetail: resolve(__dirname, 'blog-detail.html'),
+        contact: resolve(__dirname, 'contact.html')
       }
     }
   }
